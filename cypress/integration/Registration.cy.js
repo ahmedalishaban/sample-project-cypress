4b0e55219d074b4adf8f518ce6c1d2e615ff018c
AhmedAli <eng.ahmedali05@gmail.com>
2023-04-12 06:30:42 +0200

    New Version of Cypress Project

--- cypress/integration/Registration.cy.js
+++ cypress/integration/Registration.cy.js
@@ -3,77 +3,67 @@ import { registration } from "./pages/registration_page"
 
 const registrationPage = new registration()
 
-beforeEach("Navigate to e-commerce website", function() {
-        // Navigation to the website
-        cy.visit("https://demo.nopcommerce.com/register")
+beforeEach(() => {
+    cy.visit("https://demo.nopcommerce.com/register")
 })
-describe("Registration Scenarios", function(){
 
-    it("Valid Registeration Script to e-commerce website", function(){
+describe("Registration Scenarios", () => {
 
-        // choosing MALE as A Gender 
+    it("Valid Registration Script for e-commerce website", () => {
+        // Choose MALE as the gender
         cy.get('#gender-male').click()
-        // Put his FIRST NAME
+        // Enter FIRST NAME
         registrationPage.enterFisrtName("Ahmed")
-        // Put his LAST NAME
+        // Enter LAST NAME
         registrationPage.enterLastName("Ali")
-        // Put his BirthDay
+        // Select BirthDay
         cy.get('[name="DateOfBirthDay"]').select("3")
-        // Put his BirthMonth
+        // Select BirthMonth
         cy.get('[name="DateOfBirthMonth"]').select("5")
-        // Put his BirthYear
+        // Select BirthYear
         cy.get('[name="DateOfBirthYear"]').select("2000")
-        // Enter his email
+        // Enter email
         registrationPage.enterEmail("ahmedal@gmail.com")
-        // Enter his Company Name
-        cy.get('#Company').type("Th3bestfuture")
-        // Enter his Password
+        // Enter Company Name
+        cy.get('#Company').type("Professional Tester")
+        // Enter Password
         cy.get('#Password').type("AHMEDali123")
-        // Confirm his Password
+        // Confirm Password
         cy.get('#ConfirmPassword').type("AHMEDali123")
         // Click on Register Button 
         registrationPage.registrationBtn()
-    
-          // Assert that the successful message appears with the expected color
-            cy.get('.result').should('be.visible')
+
+        // Assert that the successful message appears with the expected color
+        cy.get('.result').should('be.visible')
             .and('have.css', 'color', 'rgb(76, 177, 124)'); // Green color in RGB format
-
-        
     })
-    
-    
-    it("InValid Registeration Script to e-commerce website", function(){
-    
-        // Navigation to the website
-        cy.visit("https://demo.nopcommerce.com/register")
-        // choosing MALE as A Gender 
+
+    it("Invalid Registration Script for e-commerce website", () => {
+        // Choose MALE as the gender
         cy.get('#gender-male').click()
-        // Put his FIRST NAME
+        // Enter FIRST NAME
         registrationPage.enterFisrtName("Ahmed")
-        // Put his LAST NAME
+        // Enter LAST NAME
         registrationPage.enterLastName("Ali")
-        // Put his BirthDay
+        // Select BirthDay
         cy.get('[name="DateOfBirthDay"]').select("3")
-        // Put his BirthMonth
+        // Select BirthMonth
         cy.get('[name="DateOfBirthMonth"]').select("5")
-        // Put his BirthYear
+        // Select BirthYear
         cy.get('[name="DateOfBirthYear"]').select("2000")
-        // Enter his email
+        // Enter email
         registrationPage.enterEmail("ahmedal@gmail.com")
-        // Enter his Company Name
-        cy.get('#Company').type("Th3bestfuture")
-        // Enter his Password
+        // Enter Company Name
+        cy.get('#Company').type("Professional Tester")
+        // Enter Password
         cy.get('#Password').type("AHMEDali123")
-        // Confirm his Password
+        // Confirm Password
         cy.get('#ConfirmPassword').type("AHMEDali123")
         // Click on Register Button 
         registrationPage.registrationBtn()
-    
-         // Assert that the error message appears with the expected color
-         cy.get('.message-error > ul > li').should('be.visible')
-            .and('have.css', 'color', 'rgb(228, 67, 75)'); // Red color in RGB format
-        
-    })
 
+        // Assert that the error message appears with the expected color
+        cy.get('.message-error > ul > li').should('be.visible')
+            .and('have.css', 'color', 'rgb(228, 67, 75)'); // Red color in RGB format
+    })
 })
- 
